6cf38d3444e34756e806f2ddf98726536608b3d2
Troto <funkytroto@hotmail.com>
2014-04-13 13:31:49 +0200

    rearranging folders

--- Program Arcade/Pong/paddle.py
+++
@@ -1,8 +0,0 @@
-class paddle():
-    def __init__(self,width,height,colour,positionx,positiony,velocityx = 0):
-        self.width = width
-        self.height = height
-        self.colour = colour
-        self.positionx = positionx
-        self.positiony = positiony
-        self.velocityx = velocityx
